dbd7e80b55ecb625802d4cb429899f941de139c1
Julian <thiele.juli@gmail.com>
2022-04-29 21:54:41 +0200

    Removed all comments and formatted all files

--- ATM-Machine/AccountFactory.java
+++ ATM-Machine/AccountFactory.java
@@ -1,7 +1,6 @@
 
 public class AccountFactory extends Account {
 
-   //This code is used for creating additional accounts to add to the database.
 	public AccountFactory(String Username, int theAccountNumber, int thePIN, double theAvailableBalance,
 			double theTotalBalance, int isadmin) {
 		super(Username, theAccountNumber, thePIN, theAvailableBalance, theTotalBalance, isadmin);
@@ -9,8 +8,7 @@ public class AccountFactory extends Account {
 		setAccountNumber(theAccountNumber);
 		setPin(thePIN);
 		setAvailableBalance(theAvailableBalance);
-	      setTotalBalance(theTotalBalance);
-	      setAdmin(isadmin);
+		setTotalBalance(theTotalBalance);
+		setAdmin(isadmin);
 	}
-	}
-
+}
--- ATM-Machine/CashDispenser.java
+++ ATM-Machine/CashDispenser.java
@@ -1,50 +1,39 @@
-// CashDispenser.java
-// Represents the cash dispenser of the ATM
 
-public class CashDispenser 
-{
-   // the default initial number of bills in the cash dispenser
+public class CashDispenser {
+
    private final static int INITIAL_COUNT = 500;
-   private int count; // number of $20 bills remaining
-   
-   // no-argument CashDispenser constructor initializes count to default
-   public CashDispenser()
-   {
-      count = INITIAL_COUNT; // set count attribute to default
-   } // end CashDispenser constructor
+   private int count;
 
-   // simulates dispensing of specified amount of cash
-   public void dispenseCash(int amount)
-   {
-      int billsRequired = amount / 20; // number of $20 bills required
-      count -= billsRequired; // update the count of bills
-   } // end method dispenseCash
+   public CashDispenser() {
+      count = INITIAL_COUNT;
+   }
 
-   // indicates whether cash dispenser can dispense desired amount
-   public boolean isSufficientCashAvailable(int amount)
-   {
-      int billsRequired = amount / 20; // number of $20 bills required
+   public void dispenseCash(int amount) {
+      int billsRequired = amount / 20;
+      count -= billsRequired;
+   }
 
-      if (count >= billsRequired )
-         return true; // enough bills available
-      else 
-         return false; // not enough bills available
-   } // end method isSufficientCashAvailable
-} // end class CashDispenser
+   public boolean isSufficientCashAvailable(int amount) {
+      int billsRequired = amount / 20;
 
-
+      if (count >= billsRequired)
+         return true;
+      else
+         return false;
+   }
+}
 
 /**************************************************************************
- * (C) Copyright 1992-2014 by Deitel & Associates, Inc. and               *
- * Pearson Education, Inc. All Rights Reserved.                           *
- *                                                                        *
- * DISCLAIMER: The authors and publisher of this book have used their     *
- * best efforts in preparing the book. These efforts include the          *
- * development, research, and testing of the theories and programs        *
- * to determine their effectiveness. The authors and publisher make       *
- * no warranty of any kind, expressed or implied, with regard to these    *
+ * (C) Copyright 1992-2014 by Deitel & Associates, Inc. and *
+ * Pearson Education, Inc. All Rights Reserved. *
+ * *
+ * DISCLAIMER: The authors and publisher of this book have used their *
+ * best efforts in preparing the book. These efforts include the *
+ * development, research, and testing of the theories and programs *
+ * to determine their effectiveness. The authors and publisher make *
+ * no warranty of any kind, expressed or implied, with regard to these *
  * programs or to the documentation contained in these books. The authors *
- * and publisher shall not be liable in any event for incidental or       *
- * consequential damages in connection with, or arising out of, the       *
- * furnishing, performance, or use of these programs.                     *
+ * and publisher shall not be liable in any event for incidental or *
+ * consequential damages in connection with, or arising out of, the *
+ * furnishing, performance, or use of these programs. *
  *************************************************************************/
