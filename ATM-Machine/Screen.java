bab45cee59667d8684f03244a3df06cebfff314e
Julian 41455100+thieleju@users.noreply.github.com <thiele.juli@gmail.com>
2022-05-02 23:32:31 +0200

    Added SidePanel back button and error message

--- ATM-Machine/Screen.java
+++ ATM-Machine/Screen.java
@@ -3,45 +3,51 @@ import javax.swing.JTextField;
 import javax.swing.JLabel;
 
 import java.awt.Font;
+import java.awt.Color;
 import java.awt.BorderLayout;
 
 import Listeners.KeypadListener;
-import Listeners.ATMListener;
 
 public class Screen extends JFrame implements KeypadListener {
 
-	private Keypad keypad = new Keypad(this, 180, 160);
-	private SidePanel sidePanel = new SidePanel(160, 160);
+	private Keypad keypad;
+	private SidePanel sidePanel;
 
 	private JTextField tfTop = new JTextField(20);
-	// private JTextField tfBottom = new JTextField(20);
+	private JTextField tfBottom = new JTextField(20);
 
-	private Font tfFontTop = new Font("", Font.PLAIN, 12);
-	// private Font tfFontBottom = new Font("", Font.BOLD, 12);
+	private final Font tfFontTop = new Font("", Font.PLAIN, 12);
+	private final Font tfFontBottom = new Font("", Font.PLAIN, 12);
 
 	private ATM atm;
+	private String baseTitle;
 
-	public Screen(ATM atm, String title) {
-		super(title);
+	public Screen(ATM atm, String baseTitle) {
+		super(baseTitle);
+		this.baseTitle = baseTitle;
 		this.atm = atm;
+
+		sidePanel = new SidePanel(atm, 180, 160);
+		keypad = new Keypad(this, 180, 160);
 
 		// initialize main screen
 		setSize(1000, 1000);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-		// setup textfields
+		// setup textfield fonts
 		tfTop.setHorizontalAlignment(JLabel.CENTER);
 		tfTop.setFont(tfFontTop);
 		tfTop.setEditable(false);
-		// tfBottom.setHorizontalAlignment(JLabel.CENTER);
-		// tfBottom.setFont(tfFontBottom);
-		// tfBottom.setEditable(false);
+		tfBottom.setHorizontalAlignment(JLabel.CENTER);
+		tfBottom.setFont(tfFontBottom);
+		tfBottom.setEditable(false);
+		tfBottom.setForeground(Color.RED);
 
 		// add components to main screen
 		add(tfTop, BorderLayout.NORTH);
 		add(keypad, BorderLayout.CENTER);
 		add(sidePanel, BorderLayout.LINE_END);
-		// add(tfBottom, BorderLayout.SOUTH);
+		add(tfBottom, BorderLayout.SOUTH);
 		pack();
 		setVisible(true);
 	}
@@ -57,12 +63,25 @@ public class Screen extends JFrame implements KeypadListener {
 	}
 
 	public void setAdditionalTitle(String title) {
-		setTitle("ATM Machine - " + title);
+		setTitle(baseTitle + " - " + title);
+	}
+
+	public void setErrorMessage(String message) {
+		if (atm.isDebugMode())
+			System.out.println(message);
+
+		tfBottom.setText(message);
+	}
+
+	public void clearErrorMessage() {
+		tfBottom.setText("");
 	}
 
 	public void showLogin() {
 		tfTop.setText("Insert your credit/debit card, then enter your PIN number: ");
 		sidePanel.setLabelHTML("");
+		sidePanel.hideBackButton();
+		sidePanel.showTextField();
 	}
 
 	public void showMenu() {
@@ -72,14 +91,19 @@ public class Screen extends JFrame implements KeypadListener {
 				+ "2 - Withdrawal<br>"
 				+ "3 - Deposit<br>"
 				+ "4 - Logout");
+		sidePanel.hideBackButton();
+		sidePanel.showTextField();
 	}
 
 	public void showBalance() {
 		tfTop.setText("Balance Information of " + atm.getCurrentAccount().getUsername());
-		sidePanel.setLabelHTML("Available Balance is: <br>"
+		sidePanel.setLabelHTML("<br>"
+				+ "Available Balance is: <br>"
 				+ atm.getCurrentAccount().getAvailableBalance() + " €<br><br>"
 				+ "Total Balance: <br>"
 				+ atm.getCurrentAccount().getTotalBalance() + " €");
+		sidePanel.showBackButton();
+		sidePanel.hideTextField();
 	}
 
 	public SidePanel getSidePanel() {
--- ATM-Machine/SidePanel.java
+++ ATM-Machine/SidePanel.java
@@ -1,36 +1,57 @@
 
+import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
+import Listeners.ATMListener;
+import Listeners.ATMListener.ATM_Mode;
+
 import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.Color;
+import java.awt.Font;
 
 public class SidePanel extends JPanel {
 
   private final Color backgroundColor = Color.lightGray;
+  private final Font tfFont = new Font("", Font.BOLD, 20);
 
-  private JTextField tf = new JTextField(10);
+  private JTextField tf = new JTextField(5);
   private JLabel tl = new JLabel();
+  private JButton backButton = new JButton("Back");
 
-  public SidePanel(int width, int height) {
+  public SidePanel(ATMListener atml, int width, int height) {
     setPreferredSize(new Dimension(width, height));
     setBackground(backgroundColor);
     setLayout(new FlowLayout());
 
     tf.setEditable(false);
+    tf.setFont(tfFont);
+
+    backButton.setVisible(false);
+    backButton.addActionListener(e -> atml.atmSwitchModeAction(ATM_Mode.MENU));
+
     add(tf);
     add(tl);
+    add(backButton);
   }
 
-  // public void hideTextField() {
-  // tf.setVisible(false);
-  // }
+  public void hideTextField() {
+    tf.setVisible(false);
+  }
 
-  // public void showTextField() {
-  // tf.setVisible(true);
-  // }
+  public void showTextField() {
+    tf.setVisible(true);
+  }
+
+  public void hideBackButton() {
+    backButton.setVisible(false);
+  }
+
+  public void showBackButton() {
+    backButton.setVisible(true);
+  }
 
   public void clearTextField() {
     tf.setText("");
